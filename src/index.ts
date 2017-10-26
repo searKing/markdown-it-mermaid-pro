e13907eb1d4fd10f4e7ecdef46be3410a16d3c45
searKing <searKingChan@gmail.com>
2017-10-27 00:07:15 +0800

    [CHG]support PromiseLike interface

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-export {
-  IMarkdownItMermaidPro,
-  MarkdowItMermaidPro,
-} from './markdown-it-mermaid';
-import { mermaid_plugin } from './markdown-it-mermaid';
-export default mermaid_plugin;
+// export {
+//   IMarkdownItMermaidPro,
+//   MarkdowItMermaidPro,
+// } from './markdown-it-mermaid';
+export {mermaid2html} from "./markdown-it-mermaid-pro";
+import { mermaid_pro_plugin } from './markdown-it-mermaid-pro';
+export default mermaid_pro_plugin;
