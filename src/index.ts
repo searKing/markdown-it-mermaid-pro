146d929349fcc2b1389a2496b06d4f133a6f59ba
searKing <searKingChan@gmail.com>
2017-11-02 23:55:15 +0800

    [CHG]add test files

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 //   IMarkdownItMermaidPro,
 //   MarkdowItMermaidPro,
 // } from './markdown-it-mermaid';
-export { mermaid2html } from './markdown-it-mermaid-pro';
+export { IMermaid2htmlProOptions, mermaid2html } from './markdown-it-mermaid-pro';
 import {
   mermaid_pro_plugin,
   mermaid_pro_plugin_init_everytime,
