8e8decf348fdf0582384eea8981db917ae5bcfa8
searKing <searKingChan@gmail.com>
2017-11-04 02:27:35 +0800

    [BUG]refactor export as a function with properties

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,30 @@
 //   IMarkdownItMermaidPro,
 //   MarkdowItMermaidPro,
 // } from './markdown-it-mermaid';
-export {
-  IMermaid2htmlProOptions,
-  mermaid2html,
-} from './markdown-it-mermaid-pro';
+// export {
+//   IMermaid2htmlProOptions,
+//   mermaid2html,
+// } from './markdown-it-mermaid-pro';
 import {
+  IMermaidProPluginOptions,
+  mermaid2html,
   mermaid_pro_plugin,
   mermaid_pro_plugin_init_everytime,
 } from './markdown-it-mermaid-pro';
-export {
-  mermaid_pro_plugin_init_everytime,
-} from './markdown-it-mermaid-pro';
-export default mermaid_pro_plugin;
+// export {
+//   mermaid_pro_plugin_init_everytime,
+// } from './markdown-it-mermaid-pro';
+interface IStatefulFunction {
+  (md: any, options: IMermaidProPluginOptions): any;
+  mermaid2html: any;
+  mermaid_pro_plugin_init_everytime: any
+}
+
+
+const pluginFunc: IStatefulFunction = ((md: any, options: IMermaidProPluginOptions) => {
+  mermaid_pro_plugin(md, options)
+}) as IStatefulFunction;
+
+pluginFunc.mermaid2html = mermaid2html
+pluginFunc.mermaid_pro_plugin_init_everytime = mermaid_pro_plugin_init_everytime
+export = pluginFunc as any;
